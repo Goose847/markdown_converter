544565a45ce46230078558e1f26196a4d0904622
Connor <connorgoose.847@gmail.com>
2026-06-16 14:35:27 +0200

    Built out some data structures

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,23 @@ enum Block {
     Link,
     Paragraph,
 }
+
+struct Heading {
+    level: u8,
+    text: String,
+}
+struct List {
+    ordered: bool,
+    text: String,
+}
+struct Emphasis {
+    bold: bool,
+    text: String,
+}
+struct Link {
+    text: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
